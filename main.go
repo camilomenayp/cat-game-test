5152ac6143d15ba2dc5c0624c4ddae50f8b798ac
Camilo Menay <ext_cmenay@mercadolibre.com>
2020-01-22 05:26:30 -0300

    add tiles support and draws them ineficciently

--- main.go
+++ main.go
@@ -6,156 +6,28 @@ import (
 	"math"
 	"time"
 
-	"github.com/camilomenayp/cat-game-test/pkg/sprite"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
-type Physics struct {
-	gravity   float64
-	speed     float64
-	jumpSpeed float64
-	vel       pixel.Vec
-	ground    bool
-}
-
-type Movement struct {
-	action_name        string
-	type_name          string
-	total_sprites      int32
-	sprites            []sprite.Sprite
-	sprites_sheet      *pixel.Picture
-	can_be_interrupted bool
-}
-
-type Animation struct {
-	movement     Movement
-	rate         float64
-	counter      float64
-	sprite       *pixel.Sprite
-	actual_frame int32
-	finished     bool
-}
-
-type Entity struct {
-	animation   *Animation
-	entity_type int32
-	entity_name string
-	physics     Physics
-	rect        pixel.Rect
-	pos_x       float64
-	pos_y       float64
-}
-
-func (ent *Entity) UpdateMovement(mov Movement, vector pixel.Vec) {
-
-	if (ent.animation.finished && ent.animation.movement.action_name != "Idle") || (ent.animation.movement.can_be_interrupted && mov.action_name != ent.animation.movement.action_name) {
-		fmt.Println("[Game] [Entity:", ent.entity_name, "]", "Starting action:", mov.action_name)
-		ent.animation.movement = mov
-		ent.animation.counter = 0
-		ent.animation.actual_frame = 0
-		ent.animation.finished = false
-		ent.UpdatePhysics(vector)
-	}
-}
-func (ent *Entity) UpdatePhysics(vector pixel.Vec) {
-
-	if vector != ent.physics.vel {
-		x, y := vector.XY()
-		ent.physics.vel.X = x
-		ent.physics.vel.Y = y
-		fmt.Println("[Game] [Entity:", ent.entity_name, "]", "Physics vector updated to:(", x, y, ")")
-	}
-}
-
-func (mov Movement) GetFrame(index int32) *pixel.Sprite {
-	for _, v := range mov.sprites {
-		if v.Sprite_number == index+1 {
-			return v.Sprite
-		}
-	}
-	return nil
-}
-
-func (ent *Entity) checkMovementStatus() {
-
-	if ent.animation.actual_frame == ent.animation.movement.total_sprites-1 && ent.animation.movement.action_name != "Jump" {
-		ent.animation.finished = true
-	}
-}
-func (ent *Entity) update(dt float64) {
-	ent.animation.counter += dt
-	x := ent.pos_x
-	y := ent.pos_y
-	if ent.animation.counter >= ent.animation.rate {
-		ent.pos_x = ent.pos_x + ent.physics.vel.X*dt
-		if ent.pos_y+ent.physics.vel.Y*dt >= 300.0 && ent.animation.movement.action_name == "Jump" {
-			ent.pos_y = ent.pos_y + ent.physics.vel.Y*dt
-			ent.physics.vel.Y = ent.physics.vel.Y + ent.physics.gravity*dt
-		} else if ent.animation.movement.action_name == "Jump" {
-			ent.pos_y = 300
-			ent.animation.finished = true
-		}
-
-		ent.animation.actual_frame++
-		ent.animation.sprite = ent.animation.movement.GetFrame(ent.animation.actual_frame % (ent.animation.movement.total_sprites - 1))
-		ent.animation.counter = 0
-
-		if x != ent.pos_x || y != ent.pos_y {
-			fmt.Println("[Game] [Entity:", ent.entity_name, "]", "Position updated to:(", ent.pos_x, ent.pos_y, ")")
-			fmt.Println("[Game] [Entity:", ent.entity_name, "]", "Speed vector:(", ent.physics.vel.X, ent.physics.vel.Y, ")")
-
-		}
-	}
-}
-
-func (ent *Entity) draw(t pixel.Target) {
-	if ent.animation.sprite == nil {
-		ent.animation.sprite = ent.animation.movement.GetFrame(0)
-	}
-	ent.animation.sprite.Draw(t, pixel.IM.Scaled(pixel.ZV, 0.2).Chained(pixel.IM.Moved(pixel.V(ent.pos_x, ent.pos_y))))
-}
-
-var characterSpriteSheet sprite.SpriteSheet
-
-func createMovement(arrMovs *map[string]Movement, arrSprites *map[string][]sprite.Sprite, action string, pic *pixel.Picture, canBeInterrupted bool) {
-	mov := (*arrSprites)[action]
-	(*arrMovs)[action] = Movement{action_name: action, type_name: "cat", total_sprites: int32(len(mov)), sprites: mov, sprites_sheet: pic, can_be_interrupted: canBeInterrupted}
-}
-
-func initMovements() (map[string]Movement, error) {
-	characterSpriteSheet = sprite.LoadAllSprites("cat", "sprites.txt", "spritesheet.png")
-	arr := *characterSpriteSheet.Sprites
-	arrMovements := make(map[string]Movement, 0)
-	createMovement(&arrMovements, &arr, "Jump", characterSpriteSheet.Sprites_sheet, false)
-	createMovement(&arrMovements, &arr, "Walk", characterSpriteSheet.Sprites_sheet, true)
-	createMovement(&arrMovements, &arr, "Fall", characterSpriteSheet.Sprites_sheet, false)
-	createMovement(&arrMovements, &arr, "Hurt", characterSpriteSheet.Sprites_sheet, false)
-	createMovement(&arrMovements, &arr, "Idle", characterSpriteSheet.Sprites_sheet, true)
-	createMovement(&arrMovements, &arr, "Slide", characterSpriteSheet.Sprites_sheet, false)
-	createMovement(&arrMovements, &arr, "Run", characterSpriteSheet.Sprites_sheet, true)
-	createMovement(&arrMovements, &arr, "Dead", characterSpriteSheet.Sprites_sheet, false)
-
-	return arrMovements, nil
-}
+var characterSpriteSheet SpriteSheet
+var spriteManager SpriteManager
 
 func run() {
-	var (
-		frames = 0
-		second = time.Tick(time.Second)
-	)
 
-	cfg := pixelgl.WindowConfig{
-		Title:  "Cat game test",
-		Bounds: pixel.R(0, 0, 1024, 768),
-		VSync:  true,
-	}
+	cfg := getConfig()
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	spriteManager = SpriteManager{}
+	spriteManager.LoadSpriteSheet("cat", "sprites.txt", "spritesheet.png")
+	spriteManager.LoadSpriteSheet("tiles", "sprites.txt", "spritesheet.png")
+	mapDefinition := GetMap("level-test-1.csv")
+	characterSpriteSheet = spriteManager.GetSpriteSheet("cat")
 
 	arrMov, err := initMovements()
 	anim := &Animation{
@@ -164,7 +36,8 @@ func run() {
 		counter:      0.0,
 		actual_frame: 0,
 	}
-	characterEntity := Entity{animation: anim, entity_name: "cat", entity_type: 1, pos_x: 200, pos_y: 300, physics: Physics{gravity: -3000, speed: 1, jumpSpeed: 700}}
+	characterEntity := Entity{animation: anim, entity_name: "cat", entity_type: "player", pos_x: 200, pos_y: 214, physics: Physics{gravity: -4000, speed: 1, jumpSpeed: 700}}
+	background := CreateBackground("images/png/bg/bg.png")
 
 	if err != nil {
 		panic(err)
@@ -189,6 +62,8 @@ func run() {
 		}
 
 		canvas.Clear(colornames.Black)
+		background.Update(canvas, dt, pixel.V(characterEntity.physics.vel.X, 0))
+		mapDefinition.draw(canvas, dt)
 		imd.Clear()
 		characterEntity.update(dt)
 		characterEntity.draw(imd)
